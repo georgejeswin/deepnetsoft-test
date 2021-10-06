c9d27dc98f0fffe3a15f02fad9b2c7d1d8dad1f1
Jeswin George <jeswingeorge@Jeswins-MacBook-Air.local>
2021-10-06 16:18:27 +0530

    user registration backend complete

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User from "../models/usersModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -9,6 +11,49 @@ router.get("/", async (req, res) => {
     res.status(200).send(userList);
   } catch (error) {
     res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    let user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
+      phone: req.body.phone,
+    });
+    user = await user.save();
+
+    if (!user) return res.status(400).send("User cannot be created");
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("cannot find the user");
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+      const token = jwt.sign(
+        {
+          userId: user.id,
+        },
+        process.env.secret,
+        { expiresIn: "1w" }
+      );
+      res.status(200).send({ user: user.email, token: token });
+    } else {
+      res.status(400).send("password is wrong");
+    }
+  } catch (error) {
+    res.status(400).json({
       error: error.message,
     });
   }
