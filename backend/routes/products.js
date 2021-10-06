4adb691b751011c694dee557f7a5281d993dc35a
Jeswin George <jeswingeorge@Jeswins-MacBook-Air.local>
2021-10-06 17:30:44 +0530

    products edit added

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -56,15 +56,16 @@ router.get("/:id", async (req, res) => {
 router.post("/", uploadOptions.single("image"), async (req, res) => {
   try {
     const file = req.file;
-    if (!file) return res.status(400).send("No image in the request ");
+    if (!file) return res.status(400).send("No image in the req ");
     const fileName = req.file.filename;
     const basepath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+    const imagepath = `${basePath}${fileName}`;
     let product = new Product({
       name: req.body.name,
       category: req.body.category,
       quantity: req.body.quantity,
       price: req.body.price,
-      image: `${basepath}${fileName}`,
+      image: imagepath,
     });
 
     product = await product.save();
@@ -77,6 +78,42 @@ router.post("/", uploadOptions.single("image"), async (req, res) => {
       error: error.message,
     });
   }
+});
+
+router.put("/:id", uploadOptions.single("image"), async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send("Invalid product ID");
+  }
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(400).send("Invalid product");
+
+  const file = req.file;
+  let imagepath;
+
+  if (file) {
+    const fileName = file.filename;
+    const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+    imagepath = `${basePath}${fileName}`;
+  } else {
+    imagepath = product.image;
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      category: req.body.category,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      image: imagepath,
+    },
+    { new: true }
+  );
+
+  if (!updatedProduct)
+    return res.status(500).send("The Product cannot be Update!");
+
+  res.send(updatedProduct);
 });
 
 router.delete("/:id", (req, res) => {
